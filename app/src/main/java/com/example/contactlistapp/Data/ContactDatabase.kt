d34a5d2087afd1e539f32016db687168533343bd
pasangeek <pasan.madusanka9@gmail.com>
2023-10-05 22:07:59 +0900

    database added

---
+++ app/src/main/java/com/example/contactlistapp/Data/ContactDatabase.kt
@@ -0,0 +1,35 @@
+package com.example.contactlistapp.Data
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [Contact::class], version = 1, exportSchema = false)
+abstract class ContactDatabase :RoomDatabase (){
+
+    abstract fun userDao(): ContactDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: ContactDatabase? = null
+
+        fun getDatabase(context: Context): ContactDatabase{
+            val tempInstance = INSTANCE
+            if(tempInstance != null){
+                return tempInstance
+            }
+            synchronized(this){
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    ContactDatabase::class.java,
+                    "user_database"
+                ).build()
+                INSTANCE = instance
+                return instance
+            }
+        }
+    }
+
+
+}
